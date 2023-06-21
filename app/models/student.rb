c876297a17f3e6be08cbc28f44d6099d76a63ae9
Najma <najmathande@gmail.com>
2023-06-21 23:50:42 +0300

    completed assignment

---
+++ app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ApplicationRecord
+    def to_s
+        "#{first_name} #{last_name}"
+    end
+end
+
+
+
+student = Student.new(first_name: "Dwayne", last_name: "Johnson", grade: 12)
+puts student.to_s
